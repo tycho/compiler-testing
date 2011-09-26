d6c6e6e395b3e5106e5233aedd0f2692f6994c11
Steven Noonan <steven@uplinklabs.net>
2011-09-26 04:24:58 -0700

    genericize targets a bit, print pretty headers for each
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- compare.py
+++ compare.py
@@ -17,13 +17,96 @@ common = {
 	}
 
 archs = [
-		{ 'prefix' : '', 'cflags' : [ '-m64', '-march=core2' ], 'objdump-flags' : [ '-M', 'intel-mnemonic' ] },
-		{ 'prefix' : '', 'cflags' : [ '-m32', '-march=pentium-m' ], 'objdump-flags' : [ '-M', 'intel-mnemonic' ] },
-		{ 'prefix' : 'arm-linux-gnueabi-', 'cflags' : [ '-mthumb', '-mcpu=cortex-a9', '-mfpu=neon' ] },
-		{ 'prefix' : 'arm-unknown-linux-gnueabi-', 'cflags' : [ '-mthumb', '-mcpu=cortex-a9', '-mfloat-abi=softfp', '-mfpu=neon-fp16' ] },
-		{ 'prefix' : 'powerpc-unknown-linux-gnu-', 'cflags' : [ '-mcpu=G5', '-maltivec' ] },
-		{ 'prefix' : 'mipsel-unknown-linux-gnu-', 'cflags' : [ '-march=mips64r2', '-mfp64', '-mips3d' ] },
-		{ 'prefix' : 'sparc-unknown-linux-gnu-', 'cflags' : [ '-mcpu=v9', '-mvis' ] },
+		# x86_64
+		{ 'desc' : 'x86_64 generic with sse2',
+		  'prefix' : '',
+		  'cflags' : [ '-m64', '-mtune=generic', '-msse2' ],
+		  'objdump-flags' : [ '-M', 'intel-mnemonic' ] },
+
+		{ 'desc' : 'x86_64 generic without sse2',
+		  'prefix' : '',
+		  'cflags' : [ '-m64', '-mtune=generic', '-mno-sse2' ],
+		  'objdump-flags' : [ '-M', 'intel-mnemonic' ] },
+
+		# x86
+		{ 'desc' : 'x86 with sse2',
+		  'prefix' : '',
+		  'cflags' : [ '-m32', '-mtune=i686', '-msse2' ],
+		  'objdump-flags' : [ '-M', 'intel-mnemonic' ] },
+
+		{ 'desc' : 'x86 without sse2',
+		  'prefix' : '',
+		  'cflags' : [ '-m32', '-mtune=i686', '-mno-sse2' ],
+		  'objdump-flags' : [ '-M', 'intel-mnemonic' ] },
+
+		# ARM
+		{ 'desc' : 'ARMv7 with NEON-fp16, thumb (linaro)',
+		  'prefix' : 'arm-linux-gnueabi-',
+		  'cflags' : [ '-mthumb', '-march=armv7-a', '-mfpu=neon-fp16' ]},
+
+		{ 'desc' : 'ARMv7 with NEON-fp16 (linaro)',
+		  'prefix' : 'arm-linux-gnueabi-',
+		  'cflags' : [ '-marm', '-march=armv7-a', '-mfpu=neon-fp16' ]},
+
+		{ 'desc' : 'ARMv7 with NEON (linaro)',
+		  'prefix' : 'arm-linux-gnueabi-',
+		  'cflags' : [ '-marm', '-march=armv7-a', '-mfpu=neon' ]},
+
+		{ 'desc' : 'ARMv7 with VFP (linaro)',
+		  'prefix' : 'arm-linux-gnueabi-',
+		  'cflags' : [ '-marm', '-march=armv7-a', '-mfpu=vfp' ]},
+
+		{ 'desc' : 'ARMv7 with NEON-fp16, thumb (upstream GCC)',
+		  'prefix' : 'arm-unknown-linux-gnueabi-',
+		  'cflags' : [ '-mthumb', '-march=armv7-a', '-mfpu=neon-fp16' ]},
+
+		{ 'desc' : 'ARMv7 with NEON-fp16 (upstream GCC)',
+		  'prefix' : 'arm-unknown-linux-gnueabi-',
+		  'cflags' : [ '-marm', '-march=armv7-a', '-mfpu=neon-fp16' ]},
+
+		{ 'desc' : 'ARMv7 with NEON (upstream GCC)',
+		  'prefix' : 'arm-unknown-linux-gnueabi-',
+		  'cflags' : [ '-marm', '-march=armv7-a', '-mfpu=neon' ]},
+
+		{ 'desc' : 'ARMv7 with VFP (upstream GCC)',
+		  'prefix' : 'arm-unknown-linux-gnueabi-',
+		  'cflags' : [ '-marm', '-march=armv7-a', '-mfpu=vfp' ]},
+
+		# PowerPC
+		{ 'desc' : 'PowerPC 64-bit with AltiVec',
+		  'prefix' : 'powerpc-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=powerpc64', '-maltivec' ] },
+
+		{ 'desc' : 'PowerPC 64-bit without AltiVec',
+		  'prefix' : 'powerpc-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=powerpc64', '-mno-altivec' ] },
+
+		{ 'desc' : 'PowerPC 32-bit',
+		  'prefix' : 'powerpc-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=powerpc' ] },
+
+		# MIPS
+		{ 'desc' : 'MIPS 64-bit',
+		  'prefix' : 'mipsel-unknown-linux-gnu-',
+		  'cflags' : [ '-march=mips64r2', '-mfp64', '-mips3d' ] },
+
+		# SPARC
+		{ 'desc' : 'UltraSPARC T2 32-bit with VIS',
+		  'prefix' : 'sparc-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=niagara2', '-mvis' ] },
+
+		{ 'desc' : 'UltraSPARC T2 32-bit without VIS',
+		  'prefix' : 'sparc-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=niagara2', '-mno-vis' ] },
+
+		# SPARC64
+		{ 'desc' : 'UltraSPARC T2 64-bit with VIS',
+		  'prefix' : 'sparc64-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=niagara2', '-mvis' ] },
+
+		{ 'desc' : 'UltraSPARC T2 64-bit without VIS',
+		  'prefix' : 'sparc64-unknown-linux-gnu-',
+		  'cflags' : [ '-mcpu=niagara2', '-mno-vis' ] },
 	]
 
 def exec_cmd_chain(l):
@@ -40,6 +123,8 @@ def exec_cmd_chain(l):
 def main():
 	target = sys.argv[1]
 	for a in archs:
+		print "\n%s" % ("=" * 80)
+		print a['desc']
 		obj = target + '.o'
 		# Another option for dump_mix is to just add '-Wa,-a,-an,-ad' to these cflags.
 		cc = [ a['prefix'] + 'gcc' ] + common['cflags'] + a['cflags'] + [ '-c', '-o', obj, target]
@@ -49,12 +134,15 @@ def main():
 		dump_mix = [ a['prefix'] + 'objdump' ] + dump_flags + [ '-S', obj ]
 		dump_asm = [ a['prefix'] + 'objdump' ] + dump_flags + [ '-d', obj ]
 		cmds = [ cc, dump_asm, dump_mix ]
+		print "\nCommand list:"
+		for cmd in cmds:
+			print '  %s' % (' '.join(cmd))
+		print "=" * 80
 		exec_cmd_chain(cmds)
 		try:
 			os.remove(obj)
 		except:
 			pass
-		print "=" * 80
 
 
 if __name__ == "__main__":
