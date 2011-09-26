98f92b7c9c1349a5dc51a09b13b603136c3b4bb3
Steven Noonan <steven@uplinklabs.net>
2011-09-25 22:24:37 -0700

    memcpy.c: add measurement technology
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- memcpy.c
+++ memcpy.c
@@ -2,6 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef TEST
+#include <sys/time.h>
+#include <time.h>
+#endif
 
 typedef uint32_t v4 __attribute__ ((vector_size(16), aligned(1)));
 
@@ -43,13 +47,51 @@ void *memcpy_vector(void *__restrict dst, const void *__restrict src, size_t siz
 }
 
 #ifdef TEST
+static long elapsedTime(struct timeval* start, struct timeval* end)
+{
+	return ((long) end->tv_sec * 1000000 + end->tv_usec) -
+	       ((long) start->tv_sec * 1000000 + start->tv_usec);
+}
+
 int main(int argc, char **argv)
 {
-	char buf[64];
-	char *foo = "thisisakindofsmallteststringbutitshouldwork";
-	memset(buf, 0, sizeof(buf));
-	memcpy_vector(buf, foo, strlen(foo));
-	printf("%s\n", buf);
+	const size_t mem_size = 200 * 1024 * 1024;
+	size_t i;
+	char *mem, *mem1;
+
+	size_t lim = 25;
+	struct timeval start, finish;
+	long best;
+
+	mem = (char *)valloc(mem_size);
+	mem1 = (char *)valloc(mem_size);
+
+	/* Warm-up. */
+	memcpy(mem1, mem, mem_size);
+	memcpy(mem, mem1, mem_size);
+
+	/* Test each */
+	gettimeofday(&start, NULL);
+	memcpy_vector(mem1, mem, mem_size);
+	gettimeofday(&finish, NULL);
+	best = elapsedTime(&start, &finish);
+	for (i = 0; i < lim; i++) {
+		long t;
+		gettimeofday(&start, NULL);
+		memcpy_vector(mem1, mem, mem_size);
+		gettimeofday(&finish, NULL);
+		t = elapsedTime(&start, &finish);
+		if (t < best) best = t;
+		printf("run %5lu: %10.2f MB/s\n",
+			i + 1, ((double)(mem_size / 1024 / 1024))
+			/ (t / 1000000.0));
+	}
+	printf("best time: %10.2f MB/s\n",
+		(double)(mem_size / 1024 / 1024) / (best / 1000000.0));
+
+	free(mem);
+	free(mem1);
+
 	return 0;
 }
 #endif
