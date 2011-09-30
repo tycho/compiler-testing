06f99b49ea49bbc925c9fcec5f8ec11860bb71f6
Steven Noonan <steven@uplinklabs.net>
2011-09-29 23:36:48 -0700

    euclid.c: fix indentation
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- euclid.c
+++ euclid.c
@@ -1,10 +1,10 @@
 int euclid(int i, int j)
 {
 	while(i != j) {
-       if (i > j)
-           i -= j;
-       else
-           j -= i;
-    }
+	   if (i > j)
+		   i -= j;
+	   else
+		   j -= i;
+	}
 	return i;
 }
