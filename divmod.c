0df9fa01697d78c355e89eea10f02b1c6e2ce29c
Steven Noonan <steven@uplinklabs.net>
2013-07-09 10:18:53 -0700

    divmod.c: simple test of integer division/modulus
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ divmod.c
@@ -0,0 +1,62 @@
+#include <stdint.h>
+
+uint8_t umod8(uint8_t x, uint8_t y)
+{
+	return x % y;
+}
+
+int8_t mod8(int8_t x, int8_t y)
+{
+	return x % y;
+}
+
+uint16_t umod16(uint16_t x, uint16_t y)
+{
+	return x % y;
+}
+
+int16_t mod16(int16_t x, int16_t y)
+{
+	return x % y;
+}
+
+uint32_t umod32(uint32_t x, uint32_t y)
+{
+	return x % y;
+}
+
+int32_t mod32(int32_t x, int32_t y)
+{
+	return x % y;
+}
+
+
+uint8_t udiv8(uint8_t x, uint8_t y)
+{
+	return x / y;
+}
+
+int8_t div8(int8_t x, int8_t y)
+{
+	return x / y;
+}
+
+uint16_t udiv16(uint16_t x, uint16_t y)
+{
+	return x / y;
+}
+
+int16_t div16(int16_t x, int16_t y)
+{
+	return x / y;
+}
+
+uint32_t udiv32(uint32_t x, uint32_t y)
+{
+	return x / y;
+}
+
+int32_t div32(int32_t x, int32_t y)
+{
+	return x / y;
+}
