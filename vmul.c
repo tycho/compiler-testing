adf76cc9bfda1fd4fd651e5502ab94e8caefc957
Steven Noonan <steven@uplinklabs.net>
2013-07-12 15:42:00 -0700

    vector functions: implement float32x4 variants
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- vmul.c
+++ vmul.c
@@ -1,12 +1,19 @@
 #ifdef __ARMCC_VERSION
 #include <arm_neon.h>
-typedef uint32x4_t v4;
+typedef uint32x4_t  v4i;
+typedef float32x4_t v4f;
 #else
 #include <stdint.h>
-typedef uint32_t v4 __attribute__ ((vector_size(16), aligned(1)));
+typedef uint32_t v4i __attribute__ ((vector_size(16), aligned(1)));
+typedef float    v4f __attribute__ ((vector_size(16), aligned(1)));
 #endif
 
-void vmul(v4 *o, v4 *a, v4 *b)
+void vmul4i(v4i *o, v4i *a, v4i *b)
+{
+	*o = *a * *b;
+}
+
+void vmul4f(v4f *o, v4f *a, v4f *b)
 {
 	*o = *a * *b;
 }
